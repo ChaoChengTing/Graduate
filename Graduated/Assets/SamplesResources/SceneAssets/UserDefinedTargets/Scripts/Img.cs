3b37416be2eef84634d32e80ddc1fd1b53bd286f
a104590036 <t104590036@ntut.org.tw>
2018-04-04 22:43:45 +0800

    first

---
+++ Graduated/Assets/SamplesResources/SceneAssets/UserDefinedTargets/Scripts/Img.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Img : MonoBehaviour
+{
+    public Texture2D myImg; // 從外部拖拉自己喜歡的圖片進來
+
+    private void Awake()
+    {
+        Sprite s = Sprite.Create(myImg, new Rect(0, 0, myImg.width, myImg.height), Vector2.zero);
+        GetComponent<Image>().sprite = s;
+    }
+}
